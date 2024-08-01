62553fa7773c603b1f58c351ea5e2bae17090165
jeonghoHub <tjsrsdqq4@gmail.com>
2024-08-01 23:45:07 +0900

    :sparkles: EntityNotFoundException

---
+++ src/main/java/com/study/boardappvuespring/common/exception/EntityNotFoundException.java
@@ -0,0 +1,23 @@
+package com.study.boardappvuespring.common.exception;
+
+import com.study.boardappvuespring.common.support.error.ErrorCode;
+import org.slf4j.event.Level;
+
+public class EntityNotFoundException extends BaseException {
+
+    public EntityNotFoundException(Level level, String message) {
+        super(ErrorCode.COMMON_ENTITY_NOT_FOUND, level, message);
+    }
+
+    public EntityNotFoundException(Level level) {
+        super(ErrorCode.COMMON_ENTITY_NOT_FOUND, level);
+    }
+
+    public EntityNotFoundException(String message) {
+        super(ErrorCode.COMMON_ENTITY_NOT_FOUND, message);
+    }
+
+    public EntityNotFoundException() {
+        super(ErrorCode.COMMON_ENTITY_NOT_FOUND);
+    }
+}
